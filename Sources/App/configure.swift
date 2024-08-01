d6d619a052dce44a80b02c8258f34962b01895f2
simba_mupfu <scmupfururirwa@gmail.com>
2024-08-01 05:55:06 +0200

    Add database connection details.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,9 +1,25 @@
 import Vapor
+import Fluent
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // register routes
+    
+    try app.databases.use(
+        .postgres(
+            configuration: .init(
+                hostname: "localhost",
+                username: "simba",
+                password: "",
+                database: "moviesdb",
+                tls: .disable
+            )
+        ),
+        as: .psql
+    )
+    
     try routes(app)
 }
