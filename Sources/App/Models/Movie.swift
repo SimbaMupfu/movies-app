cbfb15d7c52ccdeb985cada99f27a75bf095b5e8
simba_mupfu <scmupfururirwa@gmail.com>
2024-08-01 06:03:43 +0200

    Create the Movie class and map it to movies table.

---
+++ Sources/App/Models/Movie.swift
@@ -0,0 +1,27 @@
+//
+//  Movie.swift
+//
+//
+//  Created by Simbarashe Mupfururirwa on 2024/08/01.
+//
+
+import Foundation
+import Vapor
+import Fluent
+
+final class Movie: Model {
+    static let schema: String = "movies"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    init(){}
+    
+    init(id: UUID? = nil, title: String){
+        self.id = id
+        self.title = title
+    }
+}
