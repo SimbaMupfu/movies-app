5393cd49ce4c1a6f45cbe01ba6481ad4faab4767
simba_mupfu <scmupfururirwa@gmail.com>
2024-08-06 01:50:14 +0200

    Add routes to save and get movies.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,23 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    //create a movie
+    app.post("movies") { req async throws in
+        let movie = try req.content.decode(Movie.self)
+        try await movie.save(on: req.db)
+        return movie
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    app.get("movies") { req async throws in
+        let movies = try await Movie.query(on: req.db)
+            .all()
+        return movies
+    }
+    
+    app.get("movies", ":id") { req async throws in
+        guard let movie = try await Movie.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.badRequest)
+        }
+        return movie
     }
 }
